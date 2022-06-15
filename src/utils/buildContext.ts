75f7f40effc53073d9d79a89d7250ff285e798f5
Prince Narteh <prinarteh@gmail.com>
2022-06-15 06:38:05 +0000

    adds jwt and cookie

--- src/utils/buildContext.ts
+++ src/utils/buildContext.ts
@@ -1,3 +1,35 @@
-export function buildContext() {
-  return {};
+import { FastifyReply, FastifyRequest } from "fastify";
+import { app } from "./createServer";
+
+export async function buildContext({
+  request,
+  reply,
+  connectionParams,
+}: {
+  request?: FastifyRequest;
+  reply?: FastifyReply;
+  connectionParams: {
+    Authorization: string;
+  };
+}) {
+  if (connectionParams || !request) {
+    try {
+      return {
+        user: app.jwt.verify(connectionParams?.Authorization || ""),
+      };
+    } catch (error) {
+      return { user: null };
+    }
+  }
+
+  try {
+    const user = await request.jwtVerify();
+    return { request, reply, user };
+  } catch (error) {
+    return {
+      request,
+      reply,
+      user: null,
+    };
+  }
 }
--- src/utils/createServer.ts
+++ src/utils/createServer.ts
@@ -1,13 +1,41 @@
 import "reflect-metadata";
+import fastify from "fastify";
 import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
 import { ApolloServer } from "apollo-server-fastify";
-import fastify from "fastify";
+import { execute, GraphQLSchema, subscribe } from "graphql";
+import { SubscriptionServer } from "subscriptions-transport-ws";
 import { buildSchema } from "type-graphql";
+import fastifyCors from "@fastify/cors";
+import fastifyCookie from "@fastify/cookie";
+import fastifyJwt from "@fastify/jwt";
+
 import UserResolver from "../modules/user/user.resolver";
 import { buildContext } from "./buildContext";
 import { fastifyAppClosePlugin } from "./plugins";
 
-const app = fastify();
+export const app = fastify();
+
+app.register(fastifyCors, {
+  credentials: true,
+  origin: (origin, cb) => {
+    if (["http://localhost:3000"].includes(origin)) {
+      return cb(null, true);
+    }
+    return cb(new Error("Not Allowed"), false);
+  },
+});
+
+app.register(fastifyCookie, {
+  parseOptions: {},
+});
+
+app.register(fastifyJwt, {
+  secret: "this-is-the-secret",
+  cookie: {
+    cookieName: "token",
+    signed: false,
+  },
+});
 
 export async function createServer() {
   const schema = await buildSchema({
@@ -20,11 +48,36 @@ export async function createServer() {
       fastifyAppClosePlugin(app),
       ApolloServerPluginDrainHttpServer({ httpServer: app.server }),
     ],
-    context: buildContext(),
+    context: buildContext,
   });
+
+  subscriptionServer({ schema, server: app.server });
 
   return {
     app,
     server,
   };
 }
+
+const subscriptionServer = ({
+  schema,
+  server,
+}: {
+  schema: GraphQLSchema;
+  server: ApolloServer;
+}) => {
+  return SubscriptionServer.create(
+    {
+      schema,
+      execute,
+      subscribe,
+      async onConnect(connectionParams: { Authorization: string }) {
+        return buildContext({ connectionParams });
+      },
+    },
+    {
+      server,
+      path: "/graphql",
+    }
+  );
+};
